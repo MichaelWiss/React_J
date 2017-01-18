beeff029e404cd58c02806218c873f92d76a85ed
MichaelWiss <michael.wiss@gmail.com>
2017-01-18 12:56:04 -0600

    Nav styling

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,36 +1,31 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-var Nav = React.createClass({
-
-	   render: function () {
+var Nav = () => {
    	  return(
 		<div className="top-bar">
 		 <div className="top-bar-left">
 		   <ul className="menu">
 		   <li className="menu-text">React Time App</li>
 		    <li>
-               <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+               <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink>
             </li>
             <li>
-               <Link to="/Timer" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</Link>
-            </li>
-            <li>
-               <Link to="/Countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
+               <Link to="/Countdown" activeClassName="active-link" activeStyle={{fontWeight: 'bold'}}>Countdown</Link>
             </li>
 		   </ul>
          </div>
+
 		  <div className="top-bar-right">
-             <ul>
+            <ul className="menu">
               <li className="menu-text">
-              created by Michael Wiss
+                  created by<a href="http://resume.michaelrwiss.com/" target="_blank">Michael Wiss</a>
               </li>
-             </ul>
-              </div>
+            </ul>
+           </div>
 	   </div>
     );
-   }
-});
+};
 
 
 module.exports = Nav;
