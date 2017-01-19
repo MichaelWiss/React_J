0f243d47583c56bfd571f0a949e3a2087d69755a
MichaelWiss <michael.wiss@gmail.com>
2017-01-18 19:18:33 -0600

    1st test passed

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ var webpackConfig = require('./webpack.config');
 
 module.exports = function (config) {
 	config.set({
-      browsers: ['Chome'],
+      browsers: ['Chrome'],
       singleRun: true,
       frameworks: ['mocha'],
       files: ['app/tests/**/*.test.jsx'],
