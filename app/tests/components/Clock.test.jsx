d2020e803c1d3f3e610d6ccc796aa128e6ebb47a
MichaelWiss <michael.wiss@gmail.com>
2017-01-22 12:59:11 -0600

    clock test

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -1,4 +1,14 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
 
+
+var Clock = require('Clock');
+
+describe('Clock', ()=> {
+	it('should exist', () => {
+		expect(Clock).toExist();
+	});
+});
