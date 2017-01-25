b2703082315597656cb5765fbb33508c4bd876cf
MichaelWiss <michael.wiss@gmail.com>
2017-01-25 14:14:06 -0600

    css

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -6,7 +6,7 @@ var Main = (props) => {
 		<div>
 		    <Nav/>
 			<div className="row">
-			 <div className="large-4 medium-6 small-centered columns">
+			 <div className="column small-centered medium-6 large-4">
 			  {props.children}
 			 </div>
 		    </div>
