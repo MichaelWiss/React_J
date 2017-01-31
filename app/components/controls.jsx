74e28799cdfc46e325a7cb7d14dad073f5afb2f7
MichaelWiss <michael.wiss@gmail.com>
2017-01-31 17:51:32 -0600

    button component

--- app/components/controls.jsx
+++ app/components/controls.jsx
@@ -3,6 +3,20 @@ var React = require('react');
 var Controls = React.creaClass({
     propTypes: {
     	countDownStatus: React.PropTypes.string.isRequired
+    },
+    render: function () {
+    	var {countdownStatus} = this.props;
+    	var renderStartStopButton = () => {
+            if (countdownStatus === 'started') {
+            	return <button className="button secondary"></button>
+            } else if (countdownStatus === 'paused') {
+            	return <button className="button primary">Start</button>
+            }
+    	};
+
+    	return (
+    		<button className="button alert hollow">Clear</button>
+      )
     }
 });
 
