79eae574ea6575e99212ca604830d389ec82e28e
MichaelWiss <michael.wiss@gmail.com>
2017-01-31 17:41:21 -0600

    controls component

--- app/components/controls.jsx
+++ app/components/controls.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 
 var Controls = React.creaClass({
-
+    propTypes: {
+    	countDownStatus: React.PropTypes.string.isRequired
+    }
 });
 
 module.exports = Controls;
